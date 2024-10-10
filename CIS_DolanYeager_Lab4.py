e9450109716f89e81a9de3838956e7aa936f5333
dyeager6 <dyeager6@mail.pima.edu>
2024-10-09 19:47:16 -0700

    Update CIS_DolanYeager_Lab4.py
    Revised Python file after it was imported incorrectly. Copied and pasted code from original file.

--- CIS_DolanYeager_Lab4.py
+++ CIS_DolanYeager_Lab4.py
@@ -1,105 +1,46 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 30,
-   "id": "489e4a50-ce7d-4f7b-be43-df1cb8e5f794",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdin",
-     "output_type": "stream",
-     "text": [
-      "Enter the monthly sales amount: $ 125000\n",
-      "Enter percentage of sales increase: 4.5\n"
-     ]
-    },
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "The store bonus amount is $ 6000\n",
-      "The employee bonus amount is $ 50\n"
-     ]
-    }
-   ],
-   "source": [
-    "# Module 4 Lab-4\n",
-    "# Dolan Yeager\n",
-    "# 10/9/24\n",
-    "# This program will calculate store bonus based on monthly sales and employee bonus based on sales increase\n",
-    "\n",
-    "# declare local variables\n",
-    "monthlySales = 0  # monthly sales amount\n",
-    "storeAmount = 0  # store bonus amount\n",
-    "empAmount = 0  # employee bonus amount\n",
-    "salesIncrease = 0  # percent of sales increase\n",
-    "prompt = ('Enter the monthly sales amount: $') # prompt will be a string literal\n",
-    "\n",
-    "# This code gets the monthly sales\n",
-    "monthlySales = float(input(prompt))\n",
-    "\n",
-    "# This code determines the storeAmount bonus\n",
-    "if monthlySales >= 110000:\n",
-    "    storeAmount = 6000\n",
-    "elif monthlySales >= 100000:\n",
-    "    storeAmount = 5000\n",
-    "elif monthlySales >= 90000:\n",
-    "    storeAmount = 4000\n",
-    "elif monthlySales >= 80000:\n",
-    "    storeAmount = 3000\n",
-    "else:\n",
-    "    storeAmount = 0\n",
-    "\n",
-    "# This code gets the percent of increase in sales\n",
-    "salesIncrease = float(input('Enter percentage of sales increase:'))\n",
-    "salesIncrease = salesIncrease / 100\n",
-    "\n",
-    "# This code determines the empAmount bonus\n",
-    "if salesIncrease >= .05:\n",
-    "\tempAmount = 75\n",
-    "elif salesIncrease >= .04:\n",
-    "\tempAmount = 50\n",
-    "elif salesIncrease >= .03:\n",
-    "\tempAmount = 40\n",
-    "else:\n",
-    "\tempAmount = 0\n",
-    "        \n",
-    "# This code prints the bonus information\n",
-    "print('The store bonus amount is $', storeAmount)\n",
-    "print('The employee bonus amount is $', empAmount)\n",
-    "if (storeAmount == 6000 ) and (empAmount == 75):\n",
-    "\tprint('Congrats! You have reached the highest bonus amounts possible! ')\n"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "fa3e12bd-9186-4fe0-b6c3-86bce9292f7f",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.12.4"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+# Module 4 Lab-4
+# Dolan Yeager
+# 10/9/24
+# This program will calculate store bonus based on monthly sales and employee bonus based on sales increase
+
+# declare local variables
+monthlySales = 0  # monthly sales amount
+storeAmount = 0  # store bonus amount
+empAmount = 0  # employee bonus amount
+salesIncrease = 0  # percent of sales increase
+prompt = ('Enter the monthly sales amount: $') # prompt will be a string literal
+
+# This code gets the monthly sales
+monthlySales = float(input(prompt))
+
+# This code determines the storeAmount bonus
+if monthlySales >= 110000:
+    storeAmount = 6000
+elif monthlySales >= 100000:
+    storeAmount = 5000
+elif monthlySales >= 90000:
+    storeAmount = 4000
+elif monthlySales >= 80000:
+    storeAmount = 3000
+else:
+    storeAmount = 0
+
+# This code gets the percent of increase in sales
+salesIncrease = float(input('Enter percentage of sales increase:'))
+salesIncrease = salesIncrease / 100
+
+# This code determines the empAmount bonus
+if salesIncrease >= .05:
+	empAmount = 75
+elif salesIncrease >= .04:
+	empAmount = 50
+elif salesIncrease >= .03:
+	empAmount = 40
+else:
+	empAmount = 0
+        
+# This code prints the bonus information
+print('The store bonus amount is $', storeAmount)
+print('The employee bonus amount is $', empAmount)
+if (storeAmount == 6000 ) and (empAmount == 75):
+	print('Congrats! You have reached the highest bonus amounts possible! ')
